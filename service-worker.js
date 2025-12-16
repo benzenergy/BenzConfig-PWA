b9f120ba9183f9bfa1de9c296c43665c6b06b530
Vadim Chekaev <161916636+benzenergy@users.noreply.github.com>
2025-12-16 09:43:12 +0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'benzconfig-cache-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/app.js',
+    '/manifest.json',
+    '/icon-180.png'
+];
+
+// Установка
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// Активация
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
